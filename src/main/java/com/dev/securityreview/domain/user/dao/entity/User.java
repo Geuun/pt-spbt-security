15e11f06a436028c50a547c09d6afc632c869b45
KimJaeGeun <jgk9612@gmail.com>
2022-12-06 14:40:37 +0900

    feat: Add Spring Layers
    1. dao layer 추가
    2. repository layer 추가
    3. service layer 추가

--- src/main/java/com/dev/securityreview/domain/user/dao/entity/User.java
+++ src/main/java/com/dev/securityreview/domain/user/dao/entity/User.java
@@ -1,2 +1,24 @@
-package com.dev.securityreview.domain.user.dao.entity;public class User {
+package com.dev.securityreview.domain.user.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userName;
+    private String password;
+    private String emailAddress;
 }
--- src/main/java/com/dev/securityreview/domain/user/service/UserService.java
+++ src/main/java/com/dev/securityreview/domain/user/service/UserService.java
@@ -1,2 +1,12 @@
-package com.dev.securityreview.domain.user.service;public class UserService {
+package com.dev.securityreview.domain.user.service;
+
+import com.dev.securityreview.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
 }
