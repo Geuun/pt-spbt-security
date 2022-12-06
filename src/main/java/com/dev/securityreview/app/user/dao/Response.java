5b04eec9a25a19aceccd2c0f0c6b794b67c518eb
KimJaeGeun <jgk9612@gmail.com>
2022-12-06 17:46:26 +0900

    feat: Add Response for wrapping all Res

---
+++ src/main/java/com/dev/securityreview/app/user/dao/Response.java
@@ -0,0 +1,21 @@
+package com.dev.securityreview.app.user.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response<T> {
+    private String resultCode;
+    private T result;
+
+    public static Response<Void> error(String resultCode) {
+        return new Response(resultCode, null);
+    }
+
+    public static <T> Response<T> success(T result) {       // 와일드 카드 적용
+        return new Response("SUCCESS", result);
+    }
+}
