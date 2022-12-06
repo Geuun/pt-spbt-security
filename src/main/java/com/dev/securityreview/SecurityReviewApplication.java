0699e91ebea0f56238e1ba36c743102f43caf2a9
KimJaeGeun <jgk9612@gmail.com>
2022-12-06 14:18:09 +0900

    Inatial commit

---
+++ src/main/java/com/dev/securityreview/SecurityReviewApplication.java
@@ -0,0 +1,13 @@
+package com.dev.securityreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/securityreview/SecurityReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.securityreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
